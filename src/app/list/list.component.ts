e2d3c657837e880171d0271ae914f52fbb184b22
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-28 00:25:54 +0300

    clear dummy variables

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,54 +1,34 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
 import { ActivatedRoute } from '@angular/router';
-
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-} from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 interface Item {
   books: {
     title: string;
   };
 }
+
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-  xxxx: any;
-  items: any;
-  routeParams = this.route.snapshot.paramMap;
-  logg = String(this.routeParams.get('userId'));
-
   constructor(
-    private http: HttpClient,
     private route: ActivatedRoute,
     private firestore: AngularFirestore
   ) {
-    this.items = firestore.collection(this.logg).snapshotChanges();
+    this.items = firestore.collection(this.userID).snapshotChanges();
   }
 
-  @Output() selectedBook = new EventEmitter<any>();
-
-  log() {
-    this.items.subscribe((res: any) => {
-      this.xxxx = res;
-      console.log(res[0].payload.doc.data());
-    });
-  }
-
-  loggerBook(value: string) {
-    this.selectedBook.emit(value);
-  }
-
-  loggerxox() {
-    console.log('list1111', this.list);
-  }
+  items: any;
+  routeParams = this.route.snapshot.paramMap;
+  userID = String(this.routeParams.get('userId'));
 
   @Input() list: any;
+  @Output() selectedBook = new EventEmitter<any>();
+
+  selectBook(value: string) {
+    this.selectedBook.emit(value);
+  }
 }
