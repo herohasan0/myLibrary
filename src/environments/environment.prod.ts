c7455c4dc37e59be9b6a7b42a429cf4ec1e5ca47
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-27 18:41:34 +0300

    edit env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCxBuQ6h0Yj-0if2sZyltYHdlnqiJRwsnU',
+    authDomain: 'library-c46f1.firebaseapp.com',
+    databaseURL: '<your-database-URL>',
+    projectId: 'library-c46f1',
+    storageBucket: 'library-c46f1.appspot.com',
+    messagingSenderId: '271071552344',
+    appId: '1:271071552344:web:82739fba5e420e3927fb2d',
+  },
 };
