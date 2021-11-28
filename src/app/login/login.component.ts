d7eec4bb8c1d8a2856d612212b11dd12d20bb98a
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-11-29 00:32:24 +0300

    change login and register form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
 import { LibraryService } from '../library.service';
 
 @Component({
@@ -10,40 +8,9 @@ import { LibraryService } from '../library.service';
 })
 export class LoginComponent implements OnInit {
   constructor(private libraryService: LibraryService) {}
-  hide = true;
 
   SignIn(username: string, password: string) {
     this.libraryService.SignIn(username, password);
-  }
-
-  userForm = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(4),
-    ]),
-  });
-
-  get userEmail() {
-    return this.userForm.get('email');
-  }
-
-  get userPassword() {
-    return this.userForm.get('password');
-  }
-
-  emailError() {
-    if (this.userEmail?.hasError('email')) {
-      return 'You should enter a valid email.';
-    }
-    return 'Email is required.';
-  }
-
-  passwordError() {
-    if (this.userPassword?.hasError('minlength')) {
-      return 'Password must be at least 4 characters long.';
-    }
-    return 'Password is required.';
   }
 
   ngOnInit(): void {}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,14 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
-import { FormControl, Validators } from '@angular/forms';
-
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import {
-  AngularFirestore,
-  AngularFirestoreDocument,
-} from '@angular/fire/compat/firestore';
-
-import { Router } from '@angular/router';
+import { LibraryService } from '../library.service';
 
 @Component({
   selector: 'app-register',
@@ -16,58 +7,11 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-  hide = true;
-  constructor(
-    public afs: AngularFirestore,
-    public afAuth: AngularFireAuth, // Inject Firebase auth service
-    private firestore: AngularFirestore,
-    public router: Router
-  ) {}
+  constructor(private libraryService: LibraryService) {}
 
-  uid: any = '';
-
-  SignUp(email: string, password: string) {
-    return this.afAuth
-      .createUserWithEmailAndPassword(email, password)
-      .then((result) => {
-        this.uid = result.user?.uid;
-        console.log('result.user', result.user && result.user.uid);
-        this.firestore.collection(this.uid).add({});
-        this.router.navigate([`user/${this.uid}`]);
-      })
-      .catch((err) => {
-        console.log(err.message);
-      });
+  SignUp(username: string, password: string) {
+    this.libraryService.SignUp(username, password);
   }
-
-  //////////
-
-  email = new FormControl('', [Validators.required, Validators.email]);
-
-  password = new FormControl('', [
-    Validators.required,
-    Validators.minLength(10),
-  ]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-
-  getErrorMessageForPass() {
-    if (this.password.hasError('required')) {
-      return 'You must enter a value';
-    }
-    if (this.password.hasError('minLength')) {
-      return 'You must enter ten character at least';
-    }
-    return this.password.hasError('password') ? 'Deneem' : 'Hasan';
-  }
-
-  //////////
 
   ngOnInit(): void {}
 }
